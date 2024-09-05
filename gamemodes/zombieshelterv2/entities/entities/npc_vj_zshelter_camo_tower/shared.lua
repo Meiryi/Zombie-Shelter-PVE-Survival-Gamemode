704055b7ee6a2d534456bce710e8d7691f955115
Meiryi <chingchong7410@gmail.com>
2024-09-05 09:30:33 +0800

    Revert "Add files via upload"
    This reverts commit 37367bb2d98df846d3d16a71e7192a50f71153af.

--- gamemodes/zombieshelterv2/entities/entities/npc_vj_zshelter_camo_tower/shared.lua
+++
@@ -1,8 +0,0 @@
-ENT.Base 			= "base_gmodentity"
-ENT.Type 			= "anim"
-ENT.PrintName 		= "ZShelter Mortar Cannon"
-ENT.Author 			= "Meika"
-ENT.Contact 		= ""
-ENT.Purpose 		= "Spawn it and fight with it!"
-ENT.Instructions 	= "Click on the spawnicon to spawn it."
-ENT.Category		= "ZShelter"
